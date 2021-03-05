151a3f50993650ba7ced5aeaba3c4eff9a75219a
Matthias Antholzer <Matthias.antholzer@t-online.de>
2021-03-05 16:47:12 +0100

    Datenbankvorbereitung

--- app/src/main/java/com/example/namenlosetrinkspielapp/MainActivity.java
+++ app/src/main/java/com/example/namenlosetrinkspielapp/MainActivity.java
@@ -3,6 +3,13 @@ package com.example.namenlosetrinkspielapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +17,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("test");
     }
+
 }
