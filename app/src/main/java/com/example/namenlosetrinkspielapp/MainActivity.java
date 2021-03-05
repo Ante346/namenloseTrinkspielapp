926f6ad3263093447609b68bd8ae36b945635f37
Matthias Antholzer <Matthias.antholzer@t-online.de>
2021-03-05 17:41:02 +0100

    Datenbankanbindung
    Die Anwendung kann nun die Tasks von der Datenbank herunterladen

--- app/src/main/java/com/example/namenlosetrinkspielapp/MainActivity.java
+++ app/src/main/java/com/example/namenlosetrinkspielapp/MainActivity.java
@@ -1,22 +1,50 @@
 package com.example.namenlosetrinkspielapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.util.Log;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        db = FirebaseFirestore.getInstance();
+
+        getTasks();
+    }
+
+    private void getTasks() {
+        db.collection("Tasks")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d("Success", document.getId() + " => " + document.getData());
+                            }
+                        } else {
+                            Log.d("Fail", "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+    }
+
+    private void getNewTask() {
+
     }
 
 }
