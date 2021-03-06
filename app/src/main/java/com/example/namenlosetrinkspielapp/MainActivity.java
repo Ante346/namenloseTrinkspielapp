5001b5ac718b7bf9c310eafb6a55d0e91c3fd3d9
Matthias Antholzer <Matthias.antholzer@t-online.de>
2021-03-06 13:26:36 +0100

    Players
    Now the players are shown on top of the screen

--- app/src/main/java/com/example/namenlosetrinkspielapp/MainActivity.java
+++ app/src/main/java/com/example/namenlosetrinkspielapp/MainActivity.java
@@ -3,11 +3,15 @@ package com.example.namenlosetrinkspielapp;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -15,7 +19,10 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,6 +30,11 @@ public class MainActivity extends AppCompatActivity {
     private FirebaseFirestore db;
     private ArrayList<com.example.namenlosetrinkspielapp.Task> tasks = new ArrayList<>();
     private int taskCount = 0;
+    private int playerCount = 0;
+
+    private ArrayList<Player> players = new ArrayList<>();
+    private MyAdapter adapter;
+    private RecyclerView mRecyclerView;
 
     //UI
     TextView nameView;
@@ -32,6 +44,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //test
+        players.add(new Player("ante"));
+        players.add(new Player("stef"));
+        players.add(new Player("paul"));
+        players.add(new Player("hamster"));
+        players.add(new Player("hias"));
+        players.add(new Player("Ott"));
+
         setUpUI();
 
         db = FirebaseFirestore.getInstance();
@@ -42,6 +63,15 @@ public class MainActivity extends AppCompatActivity {
     private void setUpUI() {
         nameView = findViewById(R.id.name_field);
         descriptionView = findViewById(R.id.description_view);
+
+        LinearLayoutManager layoutManager= new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        mRecyclerView.setLayoutManager(layoutManager);
+
+
+        adapter = new MyAdapter(this,players);
+
+        mRecyclerView.setAdapter(adapter);
     }
 
     private void getTasks() {
@@ -71,6 +101,13 @@ public class MainActivity extends AppCompatActivity {
         nameView.setText(tasks.get(taskCount).getName());
         descriptionView.setText(tasks.get(taskCount).getDescription());
         taskCount++;
+    }
+
+    public void nextCard(android.view.View view){
+
+        //TODO Abfrage ob Karten übrig sind
+
+        getNewTask();
     }
 
 }
---
+++ app/src/main/java/com/example/namenlosetrinkspielapp/Player.java
@@ -0,0 +1,13 @@
+package com.example.namenlosetrinkspielapp;
+
+public class Player {
+    String name;
+
+    public Player(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+}
